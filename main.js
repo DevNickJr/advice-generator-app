b5e65467ae3977350917da7bfbdf4ee6e235d54a
Nicholas Duadei <nicholasduadei14@gmail.com>
2022-04-08 21:56:05 +0100

    initial

---
+++ main.js
@@ -0,0 +1,27 @@
+
+let xhttp = new XMLHttpRequest();
+xhttp.onload = function() {
+    let message= JSON.parse( this.responseText);
+
+
+    document.getElementById('advice-paragraph').innerHTML= message.slip.advice;
+    document.getElementById('id-number').innerHTML= message.slip.id;  
+}
+
+
+
+function giveAdvice() {
+
+    const message = document.getElementById('advice-paragraph');
+    const id = document.getElementById('id-number');
+    let xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        if (xhttp.status = 200) {
+            const resp= JSON.parse(this.responseText);
+            message.innerHTML= resp.slip.advice
+            id.innerHTML= resp.slip.id; 
+        }    
+    }
+    xhttp.open('GET', 'https://api.adviceslip.com/advice');
+    xhttp.send();
+}
